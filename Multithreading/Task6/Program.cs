59151bfd478d34dcc23a26c824b9680d1dfc3fdb
Black Jack <chechetsasha95@gmail.com>
2018-03-19 00:33:06 +0300

    Implement task with Thread.Sleep()
    Add solution with Thread.Sleep that should be implemented without it

--- Multithreading/Task6/Program.cs
+++ Multithreading/Task6/Program.cs
@@ -2,14 +2,67 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
+using System.Threading;
 using System.Threading.Tasks;
 
 namespace Task6
 {
     class Program
     {
+        public static List<string> sharedCollection = new List<string>();
+        public static ManualResetEventSlim addEventSlim = new ManualResetEventSlim(false);
+        public static ManualResetEventSlim printEventSlim = new ManualResetEventSlim(true);
+        public static object lockObject = new object();
+
         static void Main(string[] args)
         {
+            var firstThread = new Thread(DoFirstWork);
+            var secondThread = new Thread(DoSecondWork);
+            secondThread.Start();
+            firstThread.Start();
+            Console.WriteLine("Main thread ends");
+            Console.ReadKey();
+        }
+
+        public static void DoFirstWork()
+        {
+            for (int i = 0; i < 10; i++)
+            {
+                lock (lockObject)
+                {
+                    sharedCollection.Add(i.ToString());
+                    addEventSlim.Set();
+                }
+                //TODO: Think on implementation without sleep;
+                Thread.Sleep(100);
+            }
+            printEventSlim.Reset();
+            Console.WriteLine("First thread end");
+        }
+
+        public static void DoSecondWork()
+        {
+            while (printEventSlim.IsSet)
+            {
+                if (addEventSlim.Wait(1000))
+                {
+                    lock (lockObject)
+                    {
+                        PrintCollection(sharedCollection);
+                        Console.WriteLine("Add Event Reset");
+                        addEventSlim.Reset();
+                    }
+                }
+            }
+            Console.WriteLine("Second thread end");
+        }
+
+        private static void PrintCollection(IEnumerable<object> collection)
+        {
+            foreach (var item in collection)
+            {
+                Console.Write(item.ToString() + " ");
+            }
         }
     }
 }
