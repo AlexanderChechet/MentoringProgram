9f0fa09a31f62d93953302bf65fc5b3aa7aa1773
Black Jack <chechetsasha95@gmail.com>
2018-03-21 02:03:02 +0300

    Add multithreading client and server projects

---
+++ Multithreading/ClientConsole/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Sockets;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ClientConsole
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var socket = new Socket(AddressFamily.InterNetwork, SocketType.Stream, ProtocolType.Tcp);
+            var localEndPoint = new IPEndPoint(IPAddress.Loopback, 101);
+            var serverEndPoint = new IPEndPoint(IPAddress.Loopback, 100);
+            socket.Bind(localEndPoint);
+            Console.WriteLine("Try to connect");
+            socket.Connect(serverEndPoint);
+            Console.WriteLine("Connected");
+            byte[] messageArray = new byte[100];
+            var length = socket.Receive(messageArray);
+            var message = Encoding.UTF8.GetString(messageArray);
+            Console.WriteLine($"Message recieved : {message}");
+            Console.ReadKey();
+        }
+    }
+}
---
+++ Multithreading/ServerConsole/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Sockets;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ServerConsole
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var socket = new Socket(AddressFamily.InterNetwork, SocketType.Stream, ProtocolType.Tcp);
+            var endPoint = new IPEndPoint(IPAddress.Loopback, 100);
+            socket.Bind(endPoint);
+            socket.Listen(4);
+            var client = socket.Accept();
+            Console.WriteLine("Connected");
+            var message = "Hello";
+            var messageArray = Encoding.UTF8.GetBytes(message);
+            Console.WriteLine("Message send");
+            client.Send(messageArray);
+            Console.ReadKey();
+        }
+    }
+}
