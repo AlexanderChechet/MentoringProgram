547ecb50ea1e2519c2a33f29cff42d11cddb36df
Black Jack <chechetsasha95@gmail.com>
2018-04-01 15:28:23 +0300

    Add task2

---
+++ Async I Await/Task2/DownloadItem.Designer.cs
@@ -0,0 +1,82 @@
+﻿namespace Task2
+{
+    partial class DownloadItem
+    {
+        /// <summary> 
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary> 
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Component Designer generated code
+
+        /// <summary> 
+        /// Required method for Designer support - do not modify 
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.downloadUrlLabel = new System.Windows.Forms.Label();
+            this.cancelDownloadButton = new System.Windows.Forms.Button();
+            this.progressBar1 = new System.Windows.Forms.ProgressBar();
+            this.SuspendLayout();
+            // 
+            // downloadUrlLabel
+            // 
+            this.downloadUrlLabel.AutoSize = true;
+            this.downloadUrlLabel.Location = new System.Drawing.Point(3, 0);
+            this.downloadUrlLabel.MaximumSize = new System.Drawing.Size(260, 13);
+            this.downloadUrlLabel.Name = "downloadUrlLabel";
+            this.downloadUrlLabel.Size = new System.Drawing.Size(20, 13);
+            this.downloadUrlLabel.TabIndex = 0;
+            this.downloadUrlLabel.Text = "Url";
+            // 
+            // cancelDownloadButton
+            // 
+            this.cancelDownloadButton.Location = new System.Drawing.Point(269, 3);
+            this.cancelDownloadButton.Name = "cancelDownloadButton";
+            this.cancelDownloadButton.Size = new System.Drawing.Size(78, 36);
+            this.cancelDownloadButton.TabIndex = 1;
+            this.cancelDownloadButton.Text = "Cancel";
+            this.cancelDownloadButton.UseVisualStyleBackColor = true;
+            // 
+            // progressBar1
+            // 
+            this.progressBar1.Location = new System.Drawing.Point(3, 16);
+            this.progressBar1.Name = "progressBar1";
+            this.progressBar1.Size = new System.Drawing.Size(260, 23);
+            this.progressBar1.TabIndex = 2;
+            // 
+            // DownloadItem
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.Controls.Add(this.progressBar1);
+            this.Controls.Add(this.cancelDownloadButton);
+            this.Controls.Add(this.downloadUrlLabel);
+            this.Name = "DownloadItem";
+            this.Size = new System.Drawing.Size(350, 40);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label downloadUrlLabel;
+        private System.Windows.Forms.Button cancelDownloadButton;
+        private System.Windows.Forms.ProgressBar progressBar1;
+    }
+}
---
+++ Async I Await/Task2/MainForm.Designer.cs
@@ -0,0 +1,106 @@
+﻿namespace Task2
+{
+    partial class MainForm
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.addDownloadButton = new System.Windows.Forms.Button();
+            this.downloadListPanel = new System.Windows.Forms.Panel();
+            this.label2 = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(12, 29);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(279, 20);
+            this.textBox1.TabIndex = 0;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(13, 13);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(81, 13);
+            this.label1.TabIndex = 1;
+            this.label1.Text = "Url to download";
+            // 
+            // addDownloadButton
+            // 
+            this.addDownloadButton.Location = new System.Drawing.Point(297, 27);
+            this.addDownloadButton.Name = "addDownloadButton";
+            this.addDownloadButton.Size = new System.Drawing.Size(75, 23);
+            this.addDownloadButton.TabIndex = 2;
+            this.addDownloadButton.Text = "Download";
+            this.addDownloadButton.UseVisualStyleBackColor = true;
+            this.addDownloadButton.Click += new System.EventHandler(this.addDownloadButton_Click);
+            // 
+            // downloadListPanel
+            // 
+            this.downloadListPanel.Location = new System.Drawing.Point(12, 68);
+            this.downloadListPanel.Name = "downloadListPanel";
+            this.downloadListPanel.Size = new System.Drawing.Size(360, 205);
+            this.downloadListPanel.TabIndex = 3;
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(13, 52);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(70, 13);
+            this.label2.TabIndex = 4;
+            this.label2.Text = "Download list";
+            // 
+            // MainForm
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(382, 285);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.downloadListPanel);
+            this.Controls.Add(this.addDownloadButton);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.textBox1);
+            this.Name = "MainForm";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Button addDownloadButton;
+        private System.Windows.Forms.Panel downloadListPanel;
+        private System.Windows.Forms.Label label2;
+    }
+}
+
