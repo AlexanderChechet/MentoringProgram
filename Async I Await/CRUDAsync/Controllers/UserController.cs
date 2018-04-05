e83fb2819973ac0b22bb48396e7fff4917241d9b
Black Jack <chechetsasha95@gmail.com>
2018-04-05 23:44:04 +0300

    Add getusers and usersbyid

---
+++ Async I Await/CRUDAsync/Controllers/UserController.cs
@@ -0,0 +1,96 @@
+﻿using ShopRepository.Users;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using System.Web;
+using System.Web.Mvc;
+
+namespace CRUDAsync.Controllers
+{
+    public class UserController : Controller
+    {
+        private string dbName = "ShopDb.sqlite";
+        // GET: User
+        public async Task<ActionResult> Index()
+        {
+            var repo = new UserRepository(dbName);
+            var users = await repo.GetUsers();
+            return View(users);
+        }
+
+        // GET: User/Details/5
+        public async Task<ActionResult> Details(int id)
+        {
+            var repo = new UserRepository(dbName);
+            var user = await repo.GetUserById(id);
+            return View(user);
+        }
+
+        // GET: User/Create
+        public ActionResult Create()
+        {
+            return View();
+        }
+
+        // POST: User/Create
+        [HttpPost]
+        public ActionResult Create(FormCollection collection)
+        {
+            try
+            {
+                // TODO: Add insert logic here
+
+                return RedirectToAction("Index");
+            }
+            catch
+            {
+                return View();
+            }
+        }
+
+        // GET: User/Edit/5
+        public ActionResult Edit(int id)
+        {
+            return View();
+        }
+
+        // POST: User/Edit/5
+        [HttpPost]
+        public ActionResult Edit(int id, FormCollection collection)
+        {
+            try
+            {
+                // TODO: Add update logic here
+
+                return RedirectToAction("Index");
+            }
+            catch
+            {
+                return View();
+            }
+        }
+
+        // GET: User/Delete/5
+        public ActionResult Delete(int id)
+        {
+            return View();
+        }
+
+        // POST: User/Delete/5
+        [HttpPost]
+        public ActionResult Delete(int id, FormCollection collection)
+        {
+            try
+            {
+                // TODO: Add delete logic here
+
+                return RedirectToAction("Index");
+            }
+            catch
+            {
+                return View();
+            }
+        }
+    }
+}
--- Async I Await/ShopRepository/Users/UserRepository.cs
+++ Async I Await/ShopRepository/Users/UserRepository.cs
@@ -16,17 +16,17 @@ namespace ShopRepository.Users
             connectionString = $"Data Source={name};Version=3;"; ;
         }
 
-        public User GetUserById(int id)
+        public async Task<User> GetUserById(int id)
         {
             using (var dbConnection = new SQLiteConnection(connectionString))
             {
                 User user;
-                dbConnection.Open();
+                await dbConnection.OpenAsync();
                 var sqlSelect = "SELECT id, name, surname, age FROM Users WHERE id = @Id";
                 var command = new SQLiteCommand(sqlSelect, dbConnection);
                 command.Parameters.Add(new SQLiteParameter("@Id", id));
-                var reader = command.ExecuteReader();
-                if (reader.Read())
+                var reader = await command.ExecuteReaderAsync();
+                if (await reader.ReadAsync())
                 {
                     user = new User();
                     user.Id = reader.GetInt32(0);
@@ -37,6 +37,42 @@ namespace ShopRepository.Users
                 }
             }
             return null;
+        }
+
+        public async Task<List<User>> GetUsers()
+        {
+            using (var dbConnection = new SQLiteConnection(connectionString))
+            {
+                List<User> result = new List<User>();
+                await dbConnection.OpenAsync();
+                var sqlSelect = "SELECT id, name, surname, age FROM Users";
+                var command = new SQLiteCommand(sqlSelect, dbConnection);
+                var reader = await command.ExecuteReaderAsync();
+                while (await reader.ReadAsync())
+                {
+                    var user = new User();
+                    user.Id = reader.GetInt32(0);
+                    user.Name = reader.GetString(1);
+                    user.Surname = reader.GetString(2);
+                    user.Age = reader.GetInt32(3);
+                    result.Add(user);
+                }
+                return result;
+            }
+        }
+
+        public async Task AddUser(User user)
+        {
+            using (var dbConnection = new SQLiteConnection(connectionString))
+            {
+                await dbConnection.OpenAsync();
+                var sqlInsert = "INSER INTO Users (name, surname, age) VALUES (@Name, @Surname, @Age)";
+                var command = new SQLiteCommand(sqlInsert, dbConnection);
+                command.Parameters.Add(new SQLiteParameter("@Name", user.Name));
+                command.Parameters.Add(new SQLiteParameter("@Surname", user.Surname));
+                command.Parameters.Add(new SQLiteParameter("@Age", user.Age));
+                await command.ExecuteNonQueryAsync();
+            }
         }
     }
 }
