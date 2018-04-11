32934f3a1ea85a678028532b249a133ff46ba6c4
Black Jack <chechetsasha95@gmail.com>
2018-04-12 01:01:57 +0300

    Refactor create body method

--- Expressions I IQueryable/MapperGenerator/MapperGenerator.cs
+++ Expressions I IQueryable/MapperGenerator/MapperGenerator.cs
@@ -21,47 +21,55 @@ namespace MapperGenerator
 
         private Expression GetBody<TSource, TDestination>(Expression sourceParam)
         {
-            var target = Expression.Variable(typeof(TDestination), "destination");
-            var init = Expression.New(typeof(TDestination));
-            var initTarget = Expression.Assign(target, init);
+            var bodyList = new List<Expression>();
+            var destination = Expression.Variable(typeof(TDestination), "destination");
+            var initDestination = Expression.Assign(destination, Expression.New(typeof(TDestination)));
+
+            bodyList.Add(initDestination);
+            bodyList.AddRange(CompareFields<TSource, TDestination>(sourceParam, destination));
+            bodyList.AddRange(CompareProperties<TSource, TDestination>(sourceParam, destination));
+
+            var destinationLabel = Expression.Label(typeof(TDestination));
+            var returnLabel = Expression.Label(destinationLabel, destination);
+            bodyList.Add(returnLabel);
+
+            return Expression.Block(new[] { destination }, bodyList);
+        }
+
+        private List<Expression> CompareFields<TSource, TDestination>(Expression source, Expression destination)
+        {
+            var result = new List<Expression>();
             var sourceFields = typeof(TSource).GetFields();
             var destinationFields = typeof(TDestination).GetFields();
-            CompareFields(sourceFields, destinationFields);
+            var comparer = new FieldComparer();
+            for (int i = 0; i < sourceFields.Length; i++)
+            {
+                if (!destinationFields.Contains(sourceFields[i], comparer))
+                    throw new ArgumentException("Destination object doesn't has field " + sourceFields[i].Name);
+                var destinationFieldInfo = destinationFields.First(x => comparer.Equals(x, sourceFields[i]));
+                result.Add(Expression.Assign(
+                    Expression.Field(destination, destinationFieldInfo),
+                    Expression.Field(source, sourceFields[i])));
+            }
+            return result;
+        }
+
+        private List<Expression> CompareProperties<TSource, TDestination>(Expression source, Expression destination)
+        {
+            var result = new List<Expression>();
             var sourceProperties = typeof(TSource).GetProperties();
             var destinationProperties = typeof(TDestination).GetProperties();
-            CompareProperties(sourceProperties, destinationProperties);
-            //TODO loop here
-            var asign = AssignProperty<TSource, TDestination>(sourceParam, target, sourceProperties[0], destinationProperties[0]);
-            var label = Expression.Label(typeof(TDestination));
-            var ret = Expression.Label(label, target);
-            return Expression.Block(new[] { target }, initTarget, asign, ret);
-        }
-
-        private void CompareFields(FieldInfo[] source, FieldInfo[] destination)
-        {
-            var comparer = new FieldComparer();
-            for (int i = 0; i < source.Length; i++)
-            {
-                if (!destination.Contains(source[i], comparer))
-                    throw new ArgumentException("Destination object doesn't has field " + source[i].Name);
-            }
-        }
-
-        private void CompareProperties(PropertyInfo[] source, PropertyInfo[] destination)
-        {
             var comparer = new PropertyComparer();
-            for (int i = 0; i < source.Length; i++)
+            for (int i = 0; i < sourceProperties.Length; i++)
             {
-                if (!destination.Contains(source[i], comparer))
-                    throw new ArgumentException("Destination object doesn't has field " + source[i].Name);
+                if (!destinationProperties.Contains(sourceProperties[i], comparer))
+                    throw new ArgumentException("Destination object doesn't has field " + sourceProperties[i].Name);
+                var destinationPropertyInfo = destinationProperties.First(x => comparer.Equals(x, sourceProperties[i]));
+                result.Add(Expression.Assign(
+                    Expression.Property(destination, destinationPropertyInfo),
+                    Expression.Property(source, sourceProperties[i])));
             }
-        }
-
-        private Expression AssignProperty<TSource, TDestination>(Expression source, Expression destination, PropertyInfo sourceInfo, PropertyInfo destinationInfo)
-        {
-            var destinationProperty = Expression.Property(destination, destinationInfo);
-            var sourceProperty = Expression.Property(source, sourceInfo);
-            return Expression.Assign(destinationProperty, sourceProperty);
+            return result;
         }
     }
 }
--- Expressions I IQueryable/MapperGenerator/Program.cs
+++ Expressions I IQueryable/MapperGenerator/Program.cs
@@ -13,22 +13,31 @@ namespace MapperGenerator
             var generator = new MapperGenerator();
             var mapper = generator.Generate<User, UserDto>();
             var user = new User();
-            user.Name = "Test";
+            user.Name = "TestName";
+            user.Surname = "TestSurname";
             user.Age = 1;
+            user.Id = 1;
+            user.Cash = 100.50;
             var result = mapper.Map(user);
-            Console.WriteLine(result.ToString());
             Console.ReadKey();
         }
     }
 
     public class User
     {
+        public double Cash;
+        public int Id { get; set; }
         public string Name { get; set; }
+        public string Surname { get; set; }
         public int Age { get; set; }
     }
     public class UserDto
     {
+        public double Cash;
+        public int Id { get; set; }
         public string Name { get; set; }
+        public string Surname { get; set; }
         public int Age { get; set; }
+        public int Age1 { get; set; }
     }
 }
